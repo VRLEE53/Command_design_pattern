1e26e02ac57c4dee3b76d660ff8121fd2b28ee0b
zakelguindi <89213113+zakelguindi@users.noreply.github.com>
2022-10-07 11:25:00 -0400

    input handler

--- InputHandler.java
+++ InputHandler.java
@@ -1,5 +1,14 @@
 package Command_design_pattern;
 
+import java.util.HashMap;
+
 public class InputHandler {
-    
+    private HashMap<String, Command> commands = new HashMap<String, Command>(); 
+
+    public InputHandler(Document document) {
+        
+    }
+    public void inputEntered(String data) {
+        
+    }
 }
