85be4efef9b32be17dc1598b4eb493437cf5715f
Kataleena Mishra <kitkatmishra.km@gmail.com>
2022-10-07 11:16:29 -0400

    adding beginning document code

--- Document.java
+++ Document.java
@@ -1,5 +1,31 @@
 package Command_design_pattern;
 
+import java.util.ArrayList;
+
+/**
+ * 
+ */
 public class Document {
-    
+    String fileName;
+    ArrayList<String> lines;
+
+    public Document(String fileName){
+        this.fileName = fileName;
+    }
+
+    public String view(){
+        ViewCommand.execute();
+    }
+
+    public String append(String line){
+        AppendCommand.exdcute();
+    }
+
+    public String write(String line){
+        WriteCommand.execute();
+    }
+
+    public String save(){
+        SaveCommand.execute();
+    }
 }
