c0a45f1a1dace89eee9310b64c91086665986c5f
dbkaiser25 <derekkaiser133@gmail.com>
2022-10-07 17:50:32 -0400

    changes in input handler

--- InputHandler.java
+++ InputHandler.java
@@ -4,11 +4,21 @@ import javautil.HashMap;
 
 public class InputHandler 
 {
-    private HashMap<String, command> commands;
+    /**
+     * Data type to store the input for each command in the format input (String), command
+     */
+    private HashMap<String, command> commands = new HashMap<String, Command>();
 
+    /**
+     * Initializing the Hashmap to store the String version of the Command and their respective command
+     * @param document
+     */
     public InputHandler(Document document)
     {
-
+        commands.put("save", SaveCommand(document));
+        commands.put("view", ViewCommand(document));
+        commands.put("append", AppendCommand(document));
+        commands.put("write", WriteCommand(document));
     }
 
     public void inputEntered(String data)
